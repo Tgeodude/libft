183ff802c9eeed3a8fb3d2a770ce15a653264310
Tojamura Geodude <tgeodude@ga-o1.msk.21-school.ru>
2021-10-20 19:40:41 +0300

    fsdf

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,15 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgeodude <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/20 19:36:50 by tgeodude          #+#    #+#             */
+/*   Updated: 2021/10/20 19:39:08 by tgeodude         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <stdio.h>
 #include <stdlib.h>
 #include "libft.h"
 
-void * ft_calloc( size_t number, size_t size )
+void	*ft_calloc(size_t number, size_t size)
 {
-	unsigned char *array;
-	size_t i;
+	unsigned char	*array;
+	size_t			i;
 
 	i = -1;
 	array = malloc(size * number);
-	while(i++,i < size)
-		array[i] = '0';
-	return(array);
+	if (!array)
+		return (NULL);
+	while (i++, i < number)
+		array[i] = '\0';
+	return (array);
 }
--- ft_split.c
+++ ft_split.c
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgeodude <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/20 19:12:15 by tgeodude          #+#    #+#             */
+/*   Updated: 2021/10/20 19:19:29 by tgeodude         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include "libft.h"
 
-static size_t ft_quantity(char const *s, char c)
+static size_t	ft_quantity(char const *s, char c)
 {
 	size_t	k;
 	size_t	i;
@@ -18,16 +30,17 @@ static size_t ft_quantity(char const *s, char c)
 			i++;
 		k++;
 	}
-	return(k);
+	return (k);
 }
-static size_t ft_word(char const *s, char c)
+
+static size_t	ft_word(char const *s, char c)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
 	j = 0;
-	while(s[i])
+	while (s[i])
 	{
 		while (s[i] == c && s[i])
 		{
@@ -38,10 +51,10 @@ static size_t ft_word(char const *s, char c)
 			i++;
 		return (i - j);
 	}
-	return(0);
+	return (0);
 }
 
-static void *ft_free(char **s1)
+static void	*ft_free(char **s1)
 {
 	size_t	i;
 
@@ -55,36 +68,43 @@ static void *ft_free(char **s1)
 	return (NULL);
 }
 
-char **ft_split(char const *s, char c)
+static void	*ft_splt(char const *s, char c, char **s1, size_t j)
 {
-	char **s1;
 	size_t	k;
 	size_t	i;
-    size_t  j;
 
-	if(!s)
-		return (NULL);
 	k = -1;
-    j = ft_quantity(s,c);
-	s1 = malloc((j + 1) * sizeof(char *));
-	if(!s1)
-		return (NULL);
 	while (k++, k < j)
 	{
-        i = -1;
-        s1[k] = malloc((ft_word(s,c) + 1));
+		i = -1;
+		s1[k] = malloc((ft_word(s, c) + 1));
 		if (!s1[k])
 			return (ft_free(s1));
-		while(i++,i < ft_word(s,c))
+		while (i++, i < ft_word(s, c))
 		{
-			while(*s == c && *s)
+			while (*s == c && *s)
 				s++;
 			s1[k][i] = s[i];
 		}
 		s1[k][i] = '\0';
-		while(*s != c && *s)
+		while (*s != c && *s)
 			s++;
 	}
-    s1[j] = NULL;
+	return (0);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**s1;
+	size_t	j;
+
+	if (!s)
+		return (NULL);
+	j = ft_quantity(s, c);
+	s1 = malloc((j + 1) * sizeof(char *));
+	if (!s1)
+		return (NULL);
+	ft_splt(s, c, s1, j);
+	s1[j] = NULL;
 	return (s1);
 }
