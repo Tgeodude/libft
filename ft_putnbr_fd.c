f9666b44311bf3af75356fb3c7722c90580ae2ac
Tgeodude <victorsheglow@gmail.com>
2021-10-25 17:10:38 +0300

    dfsfd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -7,25 +7,33 @@ void ft_putnbr_fd(int n, int fd)
 	int	i;
 	int	a;
 
-	j = 0;
+	j = -1;
 	i = n;
 	if (n == -2147483648)
-		write(fd, &n, 11);
+		ft_putstr_fd("-2147483648", fd);
 	else 
 	{
 		if (i < 0)
+		{
 			i = -n;
+			ft_putchar_fd('-', fd);
+		}
 		k = i;
 		while (j++, k)
 			k /= 10;
 		k = 1;
-		while (j--)
+		while (j != 0)
+		{
 			k *= 10;
-		while (k)
+			j--;
+		}
+		while (k >= 10)
 		{
 			a = ((i / k) % 10);
-			write(fd, &a, 1);
+			ft_putchar_fd((a + '0'), fd);
 			k /= 10;
 		}
+		if (k == 1)
+			ft_putchar_fd(((i % 10) + '0'), fd);
 	}
 }
